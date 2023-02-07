09c87bde3e84910489cbe2690f3fd13c8fa8f4be
Hoang An <contact@unicode.vn>
2023-02-07 14:25:06 +0700

    Module Courses - Tao form

--- modules/Courses/resources/views/add.blade.php
+++ modules/Courses/resources/views/add.blade.php
@@ -1,60 +1,186 @@
 @extends('layouts.backend')
 @section('content')
-<form action="" method="post">
-    <div class="row">
-        <div class="col-6">
-            <div class="mb-3">
-                <label for="">Tên</label>
-                <input type="text" name="name" class="form-control {{$errors->has('name')?'is-invalid':''}}" placeholder="Tên..." value="{{old('name')}}">
-                @error('name')
-                <div class="invalid-feedback">
-                    {{$message}}
-                 </div>
-                @enderror
-               
+    <form action="" method="post">
+        <div class="row">
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Tên</label>
+                    <input type="text" name="name"
+                        class="form-control title {{ $errors->has('name') ? 'is-invalid' : '' }}" placeholder="Tên..."
+                        value="{{ old('name') }}">
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Slug</label>
+                    <input type="text" name="slug"
+                        class="form-control slug {{ $errors->has('slug') ? 'is-invalid' : '' }}" placeholder="Slug..."
+                        value="{{ old('slug') }}">
+                    @error('slug')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Giảng viên</label>
+                    <select name="teacher_id" id=""
+                        class="form-select {{ $errors->has('teacher_id') ? 'is-invalid' : '' }}">
+                        <option value="0">Chọn giảng viên</option>
+                        <option value="1">Hoàng An</option>
+                    </select>
+                    @error('teacher_id')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Mã khóa học</label>
+                    <input type="text" name="code" class="form-control {{ $errors->has('code') ? 'is-invalid' : '' }}"
+                        placeholder="Mã khóa học..." id="">
+                    @error('code')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Giá khóa học</label>
+                    <input type="number" name="price"
+                        class="form-control {{ $errors->has('price') ? 'is-invalid' : '' }}" placeholder="Giá khóa học..."
+                        id="">
+                    @error('price')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Giá khuyến mãi</label>
+                    <input type="number" name="sale_price"
+                        class="form-control {{ $errors->has('sale_price') ? 'is-invalid' : '' }}"
+                        placeholder="Giá khuyến mãi..." id="">
+                    @error('sale_price')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Tài liệu đính kèm</label>
+                    <select name="is_document" id=""
+                        class="form-select {{ $errors->has('is_document') ? 'is-invalid' : '' }}">
+                        <option value="0">Không</option>
+                        <option value="1">Có</option>
+                    </select>
+                    @error('is_document')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="">Trạng thái</label>
+                    <select name="status" id=""
+                        class="form-select {{ $errors->has('status') ? 'is-invalid' : '' }}">
+                        <option value="0">Chưa ra mắt</option>
+                        <option value="1">Đã ra mắt</option>
+                    </select>
+                    @error('status')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="mb-3">
+                    <label for="">Hỗ trợ</label>
+                    <textarea name="supports" class="form-control {{ $errors->has('supports') ? 'is-invalid' : '' }}"
+                        placeholder="Hỗ trợ..."></textarea>
+                    @error('supports')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="mb-3">
+                    <label for="">Nội dung</label>
+                    <textarea name="detail" class="form-control {{ $errors->has('detail') ? 'is-invalid' : '' }}"
+                        placeholder="Nội dung..."></textarea>
+                    @error('detail')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="mb-3">
+                    <div class="row align-items-end">
+                        <div class="col-7">
+                            <label for="">Ảnh đại diện</label>
+                            <input type="text" name="thumbnail"
+                                class="form-control {{ $errors->has('thumbnail') ? 'is-invalid' : '' }}"
+                                placeholder="Ảnh đại diện..." id="">
+                            @error('thumbnail')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="col-2 d-grid">
+                            <button type="button" class="btn btn-primary">
+                                Chọn ảnh
+                            </button>
+                        </div>
+                        <div class="col-3">
+                            <img src="https://fastly.picsum.photos/id/866/1000/600.jpg?hmac=An7fELfe7MSZ868dqoFWXa4ZpYsxHrR4pjHsyvDtLJI"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Lưu lại</button>
+                <a href="{{ route('admin.users.index') }}" class="btn btn-danger">Hủy</a>
             </div>
         </div>
-        <div class="col-6">
-            <div class="mb-3">
-                <label for="">Email</label>
-                <input type="text" name="email" class="form-control {{$errors->has('email')?'is-invalid':''}}" placeholder="Email..." value="{{old('email')}}">
-                @error('email')
-                <div class="invalid-feedback">
-                    {{$message}}
-                 </div>
-                @enderror
-            </div>
-        </div>
-        <div class="col-6">
-            <div class="mb-3">
-                <label for="">Nhóm</label>
-                <select name="group_id" id="" class="form-select {{$errors->has('group_id')?'is-invalid':''}}">
-                    <option value="0">Chọn Nhóm</option>
-                    <option value="1">Administrator</option>
-                </select>
-                @error('group_id')
-                <div class="invalid-feedback">
-                    {{$message}}
-                 </div>
-                @enderror
-            </div>
-        </div>
-        <div class="col-6">
-            <div class="mb-3">
-                <label for="">Mật khẩu</label>
-                <input type="password" name="password" class="form-control {{$errors->has('password')?'is-invalid':''}}" placeholder="Mật khẩu..." id="">
-                @error('password')
-                <div class="invalid-feedback">
-                    {{$message}}
-                 </div>
-                @enderror
-            </div>
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Lưu lại</button>
-            <a href="{{route('admin.users.index')}}" class="btn btn-danger">Hủy</a>
-        </div>
-    </div>
-    @csrf
-</form>
+        @csrf
+    </form>
+@endsection
+
+@section('stylesheets')
+    <style>
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
 @endsection
--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,38 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>{{$pageTitle}} - Unicode Academy</title>
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.1/datatables.min.css"/>
 
-        <link href="{{asset('backend/css/styles.css')}}" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        @include('parts.backend.header')
-        <div id="layoutSidenav">
-            @include('parts.backend.sidebar')
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        @include('parts.backend.page_title')
-                        @yield('content')
-                    </div>
-                </main>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>{{ $pageTitle }} - Unicode Academy</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.1/datatables.min.css" />
+
+    <link href="{{ asset('backend/css/styles.css') }}" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    @yield('stylesheets')
+</head>
+
+<body class="sb-nav-fixed">
+    @include('parts.backend.header')
+    <div id="layoutSidenav">
+        @include('parts.backend.sidebar')
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    @include('parts.backend.page_title')
+                    @yield('content')
+                </div>
+            </main>
             @include('parts.backend.footer')
-            </div>
         </div>
-       
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.13.1/datatables.min.js"></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script src="{{asset('backend/js/scripts.js')}}"></script>
-        @yield('scripts')
+    </div>
 
-    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.13.1/datatables.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('backend/js/scripts.js') }}"></script>
+    @yield('scripts')
+
+</body>
+
 </html>
