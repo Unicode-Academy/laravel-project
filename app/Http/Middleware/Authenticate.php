4b7e14c0e4500305a30ac15b1b69cd831771aec5
Hoang An <contact@unicode.vn>
2023-06-01 11:54:53 +0700

    Authentication Admin P3

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -12,8 +15,22 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
+
+    protected function unauthenticated($request, array $guards)
+    {
+        if ($request->is('admin') || $request->is('admin/*')){
+            throw new AuthenticationException(
+                'Unauthenticated.',
+                $guards,
+                $this->redirectTo($request)
+            );
+        }
+       
+    }
+
     protected function redirectTo($request)
     {
+
         if (! $request->expectsJson()) {
             return route('login');
         }
--- modules/Dashboard/routes/routes.php
+++ modules/Dashboard/routes/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Modules\Dashboard\src\Http\Controllers'], function () {
+Route::group(['namespace' => 'Modules\Dashboard\src\Http\Controllers', 'middleware' => 'web'], function () {
     Route::prefix('admin')->group(function () {
         Route::get('/', 'DashboardController@index')->name('admin.dashboard');
     });
