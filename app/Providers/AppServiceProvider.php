90fb338739871038c2a6e43bdcebe7c4fa767897
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-05-25 16:36:32 +0700

    Students Auth - Phan 13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
@@ -31,6 +32,18 @@ class AppServiceProvider extends ServiceProvider
                 ->line('Hãy click vào nút bên dưới để kích hoạt tài khoản của bạn')
                 ->action('Kích hoạt tài khoản', $url)
                 ->line('Nếu bạn chưa tài khoản thì không cần làm gì');
+        });
+        ResetPassword::toMailUsing(function (object $notifiable, string $token) {
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+            return (new MailMessage)
+                ->subject('[Unicode Academy] Yêu cầu đặt lại mật khẩu')
+                ->line('Hãy click vào nút bên dưới để đặt lại mật khẩu tài khoản của bạn')
+                ->line('Chúng tôi nhận được yêu cầu đặt lại mật khẩu của bạn')
+                ->action('Đặt lại mật khẩu', $url)
+                ->line('Nếu bạn không gửi yêu cầu thì không cần làm gì cả');
         });
     }
 }
